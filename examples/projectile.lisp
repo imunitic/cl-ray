09f1faf2c9d18415008923895a81b0551c022afd
Ivica Munitić <imunitic@gmail.com>
2025-03-30 21:02:19 +0200

    RTC-CL finished projectile example program

--- examples/projectile.lisp
+++ examples/projectile.lisp
@@ -22,12 +22,16 @@
 	      (cl-ray:t+ (environment-gravity env)
 			 (environment-wind env)))))
 
-(defvar *initial-projectile* (make-projectile
-			      :position (cl-ray:create-point 0 1 0)
-			      :velocity (cl-ray:normalize (cl-ray:create-vector 1 1 0))))
-
-(defvar *initial-environment* (make-environment
-			       :gravity (cl-ray:create-vector 0 -0.1 0)
-			       :wind (cl-ray:create-vector -0.01 0 0)))
-
 ;; TODO (imunitic) :: Finish the implementation of the loop here
+(defun main ()
+  (let ((proj (make-projectile
+	    :position (cl-ray:create-point 0 1 0)
+	    :velocity (cl-ray:t* (cl-ray:normalize (cl-ray:create-vector 1 1 0)) 1)))
+	(env (make-environment
+	      :gravity (cl-ray:create-vector 0 -0.1 0)
+	      :wind (cl-ray:create-vector -0.01 0 0)))
+	(count 0))
+    (loop while (<= 0 (tuple-y (projectile-position proj))) do
+      (format t "~d :: ~a~%" count (projectile-position proj))
+      (incf count)
+      (setf proj (tick env proj)))))
